9e1d3b176b35f61233c8a12e29e21d7d5b004fc5
James Punteney <punteney@gmail.com>
2010-04-27 16:43:19 -0400

    Some wsgi config updates

--- config/wsgi/production/django_site.py
+++ config/wsgi/production/django_site.py
@@ -1,7 +1,11 @@
 import os
+import site
 import sys
 
-sys.path.appen(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))) #Moving up to the top projct dir
+# Adding the virtual_env directory
+site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../v_env/lib/python2.6/site-packages")))
+# Adding the project level directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))) #Moving up to the top projct dir
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 os.environ['DJANGO_DEBUG'] = 'False'
 os.environ['CONFIG_IDENTIFIER'] = 'production'
